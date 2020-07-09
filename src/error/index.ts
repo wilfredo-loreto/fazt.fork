c4a4c79292294a9ff33961d629feefb96b391fb1
Fernando Lopez <fer.ragnar19@gmail.com>
2020-07-09 16:18:33 -0300

    Feat/central errors (#22)
    * chore: added http-status-codes dependencie
    
    * feat: created middlewares for errors
    
    * feat: used middleware for errors
    
    * feat: used the middleware for route error
    
    * feat: deleted try catchs and only use ErrorHandle for error managment
    
    * Feat/Central-Errors (#21)
    
    * add response in user controller
    
    * [feat] add create token user and route signin
    
    * Jobs Crud (#18)
    
    * feat: add job schema and interface
    
    * feat: jobs crud done
    
    * Feature/schemas (#19)
    
    * add worshop schema and others
    
    * feat: feature controller workshop
    
    Co-authored-by: Michelyp <michelypintom@gmail.com>
    Co-authored-by: Fernando Lopez <lopezfer@globalhitss.com>
    
    Co-authored-by: Keneth Sandoval <kenethra.74@gmail.com>
    Co-authored-by: Fernando Lopez <lopezfer@globalhitss.com>
    Co-authored-by: PaoloTorregroza <paolodydtorregrosa@gmail.com>
    Co-authored-by: Michelyp <michelypintom@gmail.com>
    
    Co-authored-by: Fedeya <elfedeminaya@gmail.com>
    Co-authored-by: Keneth Sandoval <kenethra.74@gmail.com>
    Co-authored-by: Fernando Lopez <lopezfer@globalhitss.com>
    Co-authored-by: PaoloTorregroza <paolodydtorregrosa@gmail.com>
    Co-authored-by: Michelyp <michelypintom@gmail.com>

---
+++ src/error/index.ts
@@ -0,0 +1,69 @@
+import { Response, Request, NextFunction } from 'express';
+import { BAD_REQUEST, INTERNAL_SERVER_ERROR } from 'http-status-codes';
+import { MongoError } from 'mongodb';
+import { Error as MongooseError } from 'mongoose';
+import { Handler } from '../types';
+
+export class ErrorHandler extends Error {
+  statusCode: number;
+
+  constructor(statusCode: number, message: string, trace?: string) {
+    super();
+    this.statusCode = statusCode;
+    this.message = message;
+    this.stack = trace;
+  }
+}
+
+const handleError = (err: ErrorHandler, res: Response) => {
+  const { statusCode = 500, message } = err;
+  res.status(statusCode).json({
+    status: 'error',
+    statusCode,
+    message
+  });
+};
+
+export const handleErrorMiddleware = (
+  err: any,
+  _req: Request,
+  res: Response,
+  _next: NextFunction
+) => {
+  handleError(err, res);
+};
+
+const errorParse = (error: Error, next: NextFunction) => {
+  if (
+    error instanceof MongooseError.ValidationError ||
+    error instanceof MongooseError.CastError ||
+    error instanceof MongoError
+  )
+    next(new ErrorHandler(BAD_REQUEST, error.message, error.stack));
+  else if (error instanceof ErrorHandler) next(error);
+  else
+    next(
+      new ErrorHandler(
+        INTERNAL_SERVER_ERROR,
+        'Error Perfoming Action',
+        error.stack
+      )
+    );
+};
+
+export const handlerExceptionRoute = (fn: Handler): any => (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const route = fn(req, res);
+    if (route instanceof Promise) {
+      route?.catch((error: Error) => {
+        errorParse(error, next);
+      });
+    }
+  } catch (error) {
+    errorParse(error, next);
+  }
+};
--- src/routes/projects.routes.ts
+++ src/routes/projects.routes.ts
@@ -1,16 +1,19 @@
-import { Router } from "express";
+import { Router } from 'express';
 
-import * as projectCtrl from "../controllers/project.controller";
+import * as projectCtrl from '../controllers/project.controller';
+import { handlerExceptionRoute } from '../error';
 
 const router = Router();
 
-router.route('/')
-  .get(projectCtrl.getProjects)
-  .post(projectCtrl.createProject)
+router
+  .route('/')
+  .get(handlerExceptionRoute(projectCtrl.getProjects))
+  .post(handlerExceptionRoute(projectCtrl.createProject));
 
-router.route('/:id')
-  .get(projectCtrl.getProject)
-  .put(projectCtrl.updateProject)
-  .delete(projectCtrl.deleteProject)
+router
+  .route('/:id')
+  .get(handlerExceptionRoute(projectCtrl.getProject))
+  .put(handlerExceptionRoute(projectCtrl.updateProject))
+  .delete(handlerExceptionRoute(projectCtrl.deleteProject));
 
 export default router;
