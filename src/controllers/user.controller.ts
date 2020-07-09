5fe21782bb98ca7c788db2844a7a83aaa6b0f9ce
Fedeya <elfedeminaya@gmail.com>
2020-07-09 16:25:11 -0300

    refactor: removed try catchs and only use error handle

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,50 +1,38 @@
-import { Handler } from "../types";
-import User from "../models/User";
-import { success, error } from "../network/response";
+import { Handler } from '../types';
+import User from '../models/User';
+import { success, error } from '../network/response';
+import { ErrorHandler } from '../error';
+import { NOT_FOUND } from 'http-status-codes';
 
 export const getUsers: Handler = async (req, res) => {
-  try {
-    const Users = await User.find();
-    return success(res, Users, "200");
-  } catch (e) {
-    return error(res, "500", "Error getting users");
-  }
+  const Users = await User.find();
+  return success(res, Users, '200');
 };
 export const getUser: Handler = async (req, res) => {
   const user = await User.findById(req.params.id);
-  if (!user) {
-    return error(res, "404", "User not found");
-  }
+  if (!user) throw new ErrorHandler(NOT_FOUND, 'User not found');
 
-  return success(res, User, "200");
+  return success(res, User, '200');
 };
 export const createUser: Handler = async (req, res) => {
   const { nickname, email, password, firstName, lastName } = req.body;
+  const user = new User({ nickname, email, firstName, password, lastName });
+  await user.setPassword(password);
 
-  try {
-    const user = new User({ nickname, email, firstName, password, lastName });
-    await user.setPassword(password);
-
-    const newUser = await user.save();
-    console.log(newUser);
-    delete newUser.password;
-    return success(res, newUser, "201");
-  } catch (e) {
-    console.log(e);
-    return error(res, "404", "Error creating an user");
-  }
+  const newUser = await user.save();
+  console.log(newUser);
+  delete newUser.password;
+  return success(res, newUser, '201');
 };
 
 export const deleteUser: Handler = async (req, res) => {
-  try {
-    const user = await User.findById(req.params.id);
-    await User.findByIdAndRemove(req.params.id);
-    return res.status(200).json({ message: "User Deleted" });
-  } catch (e) {
-    return res.status(404).json({ message: "User not Found" });
-  }
+  const user = await User.findById(req.params.id);
+  if (!user) throw new ErrorHandler(NOT_FOUND, 'User not found');
+
+  await User.findByIdAndRemove(req.params.id);
+  return res.status(200).json({ message: 'User Deleted' });
 };
 
 export const updateUser: Handler = async (req, res) => {
-  return res.json({ message: "User Updated" });
+  return res.json({ message: 'User Updated' });
 };
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,15 +1,19 @@
 import { Router } from 'express';
-import * as userCtrl from '../controllers/user.controller'
+import * as userCtrl from '../controllers/user.controller';
+
+import { handlerExceptionRoute } from '../error';
 
 const router = Router();
 
-router.route('/')
-    .get(userCtrl.getUsers)
-    .post(userCtrl.createUser);
+router
+  .route('/')
+  .get(handlerExceptionRoute(userCtrl.getUsers))
+  .post(handlerExceptionRoute(userCtrl.createUser));
 
-router.route('/:id')
-    .get(userCtrl.getUser)
-    .put(userCtrl.updateUser)
-    .delete(userCtrl.deleteUser);
+router
+  .route('/:id')
+  .get(handlerExceptionRoute(userCtrl.getUser))
+  .put(handlerExceptionRoute(userCtrl.updateUser))
+  .delete(handlerExceptionRoute(userCtrl.deleteUser));
 
 export default router;
