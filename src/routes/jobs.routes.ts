6106d8ec98ff375b5f9557b3a539538abfd0374a
Cejas Claudio A <41306913+CejasClaudioA@users.noreply.github.com>
2020-07-12 12:41:01 -0300

    feat: documentation jobs.routes (#34)

--- src/routes/jobs.routes.ts
+++ src/routes/jobs.routes.ts
@@ -5,75 +5,137 @@ import { handlerExceptionRoute} from '../error';
 const router = Router();
 
 /**
- * @api {get} /jobs Obtiene un Job
- * @apiDescription Obtiene un Job registrado en la base de datos
+ * @api {get} /jobs Obtiene todos los trabajos
+ * @apiDescription Obtiene todos los trabajos almacenados en la base de datos.
  * @apiName GetJobs
  * @apiGroup Jobs
  *
- * @apiSuccess {String} title Title of the Job.
- * @apiSuccess {String} description Description of the Job.
- * @apiSuccess {String} employer Employer of the Job.
- * @apiSuccess {String} employerEmail Employer email of the Job.
- * @apiSuccess {String} proposals Proposals of the Employer Job .
- * @apiError Error getting jobs.
+ * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 200 OK
+ *     [
+ *      {
+ *       "_id": "dataId",
+ *       "title": " dataTitle",
+ *       "description": "dataDescription",
+ *       "employer": "dataEmployer",
+ *       "employerEmail": "dataEmployerEmail",
+ *       "proposals": [{"dataProposals"}],
+ *      },
+ *      {
+ *       "_id": "dataId2",
+ *       "title": " dataTitle2",
+ *       "description": "dataDescription2",
+ *       "employer": "dataEmployer2",
+ *       "employerEmail": "dataEmployerEmail2",
+ *       "proposals": [{"dataProposals2"}],
+ *      }
+ *     ]
+ * 
+ * @apiErrorExample {json} Error-Response:
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *       "message": "dataMessage",
+ *     }
  */
 router.get('/', handlerExceptionRoute(jobCtrl.getJobs));
 
 /**
- * @api {post} /jobs Crea un nuevo Job
- * @apiDescription Crea un nuevo Job y lo guardada en la base de datos
+ * @api {post} /jobs Crea un nuevo Trabajo
+ * @apiDescription Crea un nuevo Trabajo y lo almacena en la base de datos.
  * @apiName PostJob
- * @apiGroup Job
+ * @apiGroup Jobs
  * 
- * @apiParam (Request body) {String} title Titulo del trabajo
- * @apiParam (Request body) {String} description Descripción del trabajo
- * @apiParam (Request body) {String} employer Empleado del trabajo
- * @apiParam (Request body) {String} employerEmail Correo electrónico del empleado
- * @apiParam (Request body) {String} proposals 
+ * @apiParam (Request body) {String} title Titulo del trabajo.
+ * @apiParam (Request body) {String} description Descripción del trabajo.
+ * @apiParam (Request body) {String} employer Empleador.
+ * @apiParam (Request body) {String} employerEmail Correo electrónico del empleador.
+ * @apiParam (Request body) {[ObjectId]} proposals Propuesta/s del empleador.
  * 
- * @apiSuccess {String} title Titulo del del trabajo
- * @apiSuccess {String} description Descripcion del trabajo
- * @apiSuccess {String} employeer Empleado del trabajo
- * @apiSuccess {String} employeerEmail Correo electrónico del empleado
- * @apiError Error creating Job.
+ * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "_id": "dataId",
+ *       "title": " dataTitle",
+ *       "description": "dataDescription",
+ *       "employer": "dataEmployer",
+ *       "employerEmail": "dataEmployerEmail",
+ *       "proposals": [{"dataProposals"}],
+ *     }
+ * 
+ * @apiErrorExample {json} Error-Response:
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *       "message": "dataMessage",
+ *     }
  */
 router.post('/', handlerExceptionRoute(jobCtrl.createJob));
 
 /**
- * @api {get} /jobs/:id Obtiene un trabajo en específico.
- * @apiDescription Obtiene un trabajo en especifico de los guardados en la base de datos
+ * @api {get} /jobs/:id Obtiene un trabajo en específico
+ * @apiDescription Obtiene un trabajo en especifico de los almacenados en la base de datos.
  * @apiName GetJobID
  * @apiGroup Jobs
  *
- * @apiParam id
- *
- * @apiSuccess {String} title Title of the job.
- * @apiSuccess {String} description Description of the Job.
- * @apiSuccess {String} employeer Employeer of the job
- * @apiSuccess {String} employeerEmail E-mail of the employer.
+ * @apiParam {String} _id Identificador del objeto almacenado.
  * 
- * @apiError Job not found.
+ * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 200 OK
+ *     [
+ *      {
+ *       "_id": "dataId",
+ *       "title": " dataTitle",
+ *       "description": "dataDescription",
+ *       "employer": "dataEmployer",
+ *       "employerEmail": "dataEmployerEmail",
+ *       "proposals": [{"dataProposals"}],
+ *      }
+ *     ]
+ * 
+ * @apiErrorExample {json} Error-Response:
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *       "message": "dataMessage",
+ *     }
  */
 router.route('/:id').get(handlerExceptionRoute(jobCtrl.getJob));
 
 /**
  * @api {put} /jobs/:id Actualiza un trabajo en especifico.
- * @apiDescription Obtiene un trabajo en especifico de los guardados en la base de datos y lo actualiza.
+ * @apiDescription Obtiene un trabajo en especifico de los guardados en la base de datos y lo actualiza con el
+ * contenido del cuerpo.
  * @apiName GetJobID
  * @apiGroup Jobs
  *
- * @apiParam id
+ * @apiParam {String} _id Identificador del objeto almacenado.
  * @apiParam (Request body) {String} title Titulo del trabajo
  * @apiParam (Request body) {String} description Descripción del trabajo
  * @apiParam (Request body) {String} employeer Empleado del trabajo
  * @apiParam (Request body) {String} employerEmail Correo electrónico del empleado
- *
- * @apiSuccess {String} name Titulo del trabajo
- * @apiSuccess {String} description Descripción del trabajo
- * @apiSuccess {String} employer Empleado del trabajo
- * @apiSuccess {String} employerEmail Correo electrónico del empleado
+ * @apiParam (Request body) {[ObjectId]} proposals Propuesta/s del empleador.
  * 
- * @apiError Error updating job.
+ * @apiParamExample {json} Request-Example:
+ *     {
+ *       "title": "newMessageTitle"
+ *     }
+ *
+  * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 200 OK
+ *     [
+ *      {
+ *       "_id": "dataId",
+ *       "title": "newMessageTitle",
+ *       "description": "dataDescription",
+ *       "employer": "dataEmployer",
+ *       "employerEmail": "dataEmployerEmail",
+ *       "proposals": [{"dataProposals"}],
+ *      }
+ *     ]
+ * 
+ * @apiErrorExample {json} Error-Response:
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *       "message": "dataMessage",
+ *     }
  */
 router.route('/:id').put(handlerExceptionRoute(jobCtrl.updateJob));
 
@@ -84,14 +146,19 @@ router.route('/:id').put(handlerExceptionRoute(jobCtrl.updateJob));
  * @apiName DeleteJobID
  * @apiGroup Jobs
  *
- * @apiParam id
+ * @apiParam {String} _id Identificador del objeto.
  *
- * @apiSuccess {String} name Titulo del trabajo
- * @apiSuccess {String} description Descripción del trabajo
- * @apiSuccess {String} employer Empleado del trabajo
- * @apiSuccess {String} employerEmail Correo electrónico del trabajo
+ * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "message": "dataMessage",
+ *     }
  *  
- * @apiError Error deleting user.
+ * @apiErrorExample {json} Error-Response:
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *       "message": "dataMessage",
+ *     }
  */
 router.route('/:id').delete(handlerExceptionRoute(jobCtrl.deleteJob));
 
